0d11521e9f7cef9943bffc6738683b272024481e
Raymoo <uguu@installgentoo.com>
2017-05-09 20:01:01 -0700

    fill out and modify target

--- spell/target.lua
+++ spell/target.lua
@@ -1,3 +1,6 @@
+--- Targets
+-- @module arcana.Target
+-- @author raymoo
 
 local zero = { x = 0, y = 0, z = 0 }
 
@@ -8,11 +11,29 @@ local function metad(f)
 	return function(...)
 		local tab = f(...)
 		setmetatable(tab, Target)
+		return tab
 	end
 end
 
-Target.pos_target = metad(function(pos, dir)
-	if vector.equals(dir, zero) then
+--- Targets, passed to spell components.
+-- @section target
+
+--- Specifiers for where a spell component should apply
+-- @string type The type of target, either "pos" or "object"
+-- @tparam vector pos The targeted position
+-- @tparam ?ObjectRef ref The object, if this target is associated with one
+-- @table Target
+
+--- Functions.
+-- @section functions
+
+--- Construct a target with only a position and direction
+-- @tparam vector pos
+-- @tparam vector dir
+-- @treturn Target
+-- @function arcana.Target.pos
+Target.pos = metad(function(pos, dir)
+	if dir == nil or vector.equals(dir, zero) then
 		dir = { x = 0, y = 1, z = 0 }
 	end
 	return {
@@ -22,14 +43,21 @@ Target.pos_target = metad(function(pos, dir)
 	}
 end)
 
-Target.object_target = metad(function(ref, dir)
-	if vector.equals(dir, zero) then
+--- Construct a target using an object
+-- @tparam vector pos
+-- @tparam vector dir
+-- @tparam ObjectRef ref
+-- @treturn Target
+-- @function arcana.Target.object
+Target.object = metad(function(pos, dir, ref)
+	if dir == nil or vector.equals(dir, zero) then
 		dir = { x = 0, y = 1, z = 0 }
 	end
 	return {
 		type = "object",
-		ref = ref,
+		pos = pos,
 		dir = vector.normalize(dir),
+		ref = ref,
 	}
 end)
 
